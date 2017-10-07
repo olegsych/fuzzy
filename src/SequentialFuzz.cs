26c5eeb2fec17b4260061c5cc9482664d2af3ed7
Oleg Sych <olegsych@users.noreply.github.com>
2017-10-07 09:58:45 -0700

    Implement SequentialFuzz

---
+++ src/SequentialFuzz.cs
@@ -0,0 +1,11 @@
+﻿namespace Fuzzy
+{
+    public class SequentialFuzz : IFuzz
+    {
+        int seed;
+
+        public SequentialFuzz(int seed = default) => this.seed = seed;
+
+        public int Int32() => ++seed;
+    }
+}
---
+++ test/SequentialFuzzTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace Fuzzy
+{
+    public class SequentialFuzzTest
+    {
+        readonly IFuzz sut;
+
+        readonly int seed = new Random().Next();
+
+        public SequentialFuzzTest() => sut = new SequentialFuzz(seed);
+
+        public class Constructor : SequentialFuzzTest
+        {
+            [Fact]
+            public void DefaultsSeedToZero()
+            {
+                var expected = new SequentialFuzz(0);
+                var actual = new SequentialFuzz();
+                Assert.Equal(expected.Int32(), actual.Int32());
+            }
+        }
+
+        public class Int32 : SequentialFuzzTest
+        {
+            [Fact]
+            public void ReturnIncrementedSeedValue()
+            {
+                Assert.Equal(seed + 1, sut.Int32());
+                Assert.Equal(seed + 2, sut.Int32());
+            }
+        }
+    }
+}
