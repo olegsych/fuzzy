55c607049072c783d7f43bf0e145ffbb391f0488
Oleg Sych <olegsych@users.noreply.github.com>
2017-10-07 16:15:35 -0700

    Implement Range of hard-coded int type

---
+++ src/Range.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fuzzy
+{
+    public struct Range
+    {
+        readonly int? min;
+        readonly int? max;
+
+        Range(int? min, int? max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public int Maximum => max ?? 13;
+
+        public int Minimum => min ?? 5;
+
+        public static Range Between(int min, int max)
+        {
+            if (min > max)
+                throw new ArgumentException($"Minimum {min} is larger than maximum {max}.");
+            return new Range(min, max);
+        }
+
+        public static Range Min(int min) => new Range(min, default);
+
+        public static Range Max(int max) => new Range(default, max);
+    }
+}
---
+++ test/RangeTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xunit;
+
+namespace Fuzzy
+{
+    public class RangeTest
+    {
+        static readonly Random random = new Random();
+
+        public class Between : RangeTest
+        {
+            readonly int min = random.Next() % 1000;
+            readonly int max ;
+
+            public Between() => max = min + random.Next() % 10;
+
+            [Fact]
+            public void ReturnsCountInitializedWithGivenMinimumAndMaximumValues()
+            {
+                var sut = Range.Between(min, max);
+                Assert.Equal(min, sut.Minimum);
+                Assert.Equal(max, sut.Maximum);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenMaxIsLessThanMin()
+            {
+                var e = Assert.Throws<ArgumentException>(() => Range.Between(min, min - 1));
+                Assert.Contains(min.ToString(), e.Message);
+                Assert.Contains((min - 1).ToString(), e.Message);
+            }
+        }
+
+        public class Max : RangeTest
+        {
+            readonly int max = random.Next();
+
+            [Fact]
+            public void ReturnsCountInitializedWithGivenMaximumValue()
+            {
+                var sut = Range.Max(max);
+                Assert.Equal(max, sut.Maximum);
+            }
+        }
+
+        public class Maximum : RangeTest
+        {
+            [Fact]
+            public void DefaultsToValueAppropriateForCollections()
+            {
+                Range sut = default;
+                Assert.Equal(13, sut.Maximum);
+            }
+        }
+
+        public class Min : RangeTest
+        {
+            readonly int min = random.Next();
+
+            [Fact]
+            public void ReturnsCountInitializedWithGivenMinimumValue()
+            {
+                var sut = Range.Min(min);
+                Assert.Equal(min, sut.Minimum);
+            }
+        }
+
+        public class Minimum : RangeTest
+        {
+            [Fact]
+            public void DefaultsToValueAppropriateForCollections()
+            {
+                Range sut = default;
+                Assert.Equal(5, sut.Minimum);
+            }
+        }
+    }
+}
