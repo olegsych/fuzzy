8f7dad2bf5094ba36181aedc2b944bb1f01a2ee2
Oleg Sych <olegsych@users.noreply.github.com>
2017-10-21 09:31:41 -0700

    Create incomplete Fuzzy<T> class

---
+++ src/Fuzzy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fuzzy
+{
+    public class Fuzzy<T>
+    {
+        readonly IFuzz fuzz;
+        readonly Func<IFuzz, T> factory;
+
+        public Fuzzy(IFuzz fuzz, Func<IFuzz, T> factory)
+        {
+            this.fuzz = fuzz ?? throw new ArgumentNullException(nameof(fuzz));
+            this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
+        }
+    }
+}
---
+++ test/FuzzyTest.cs
@@ -0,0 +1,34 @@
+﻿using NSubstitute;
+using System;
+using Xunit;
+
+namespace Fuzzy
+{
+    public class FuzzyTest
+    {
+        readonly Fuzzy<TestClass> sut;
+
+        // Constructor parameters
+        readonly IFuzz fuzz = Substitute.For<IFuzz>();
+        readonly Func<IFuzz, TestClass> factory = Substitute.For<Func<IFuzz, TestClass>>();
+
+        public FuzzyTest() => sut = new Fuzzy<TestClass>(fuzz, factory);
+
+        public class Constructor : FuzzyTest
+        {
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenFuzzIsNullToFailFast()
+            {
+                Assert.Throws<ArgumentNullException>(() => new Fuzzy<TestClass>(null, factory));
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenFactoryIsNullToFailFast()
+            {
+                Assert.Throws<ArgumentNullException>(() => new Fuzzy<TestClass>(fuzz, null));
+            }
+        }
+
+        class TestClass {}
+    }
+}
