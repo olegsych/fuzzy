ddb2abe074ee1e557960dc36699bd4494ce3c59f
Oleg Sych <olegsych@users.noreply.github.com>
2018-05-26 14:34:36 -0700

    Work in progress - FuzzyArray

---
+++ src/FuzzyArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fuzzy
+{
+    public class FuzzyArray<T> : Fuzzy<T[]>
+    {
+        public FuzzyArray(IFuzz fuzz) : base(fuzz)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T[] New()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
