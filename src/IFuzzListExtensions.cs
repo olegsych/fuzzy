3abf3cc66c70bc7a1f06bfc8dbc291a6143c76aa
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-12 13:59:50 -0700

    Simplify IFuzzListExtensions

--- src/IFuzzListExtensions.cs
+++ src/IFuzzListExtensions.cs
@@ -5,13 +5,8 @@ namespace Fuzzy
 {
     public static class IFuzzListExtensions
     {
-        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, Func<T> createElement) => throw new NotImplementedException();
-        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, Func<Fuzzy<T>> createElement) => throw new NotImplementedException();
-
-        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, Func<T> createElement, Length length) => throw new NotImplementedException();
-        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, Func<Fuzzy<T>> createElement, Length length) => throw new NotImplementedException();
-
-        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, IEnumerable<T> elements) => throw new NotImplementedException();
-        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, IEnumerable<T> elements, Length length) => throw new NotImplementedException();
+        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, Func<T> createElement, Length length = default) => throw new NotImplementedException();
+        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, Func<Fuzzy<T>> createElement, Length length = default) => throw new NotImplementedException();
+        public static Fuzzy<List<T>> List<T>(this IFuzz fuzzy, IEnumerable<T> elements, Length length = default) => throw new NotImplementedException();
     }
 }
