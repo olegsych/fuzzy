40a139d2f6f3e0845193de7f84ec051af3cd4f7b
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-12 11:47:37 -0700

    Define SingleExample MVP

--- examples/SingleExample.cs
+++ examples/SingleExample.cs
@@ -7,18 +7,22 @@ namespace Fuzzy
         static readonly IFuzz fuzzy = new SequentialFuzz();
 
         [Fact]
-        public void GetValue() {
-            float d;
-            d = fuzzy.Single();
-            d = fuzzy.Single().GreaterThan(.2f);
-            d = fuzzy.Single().LessThan(.5f);
-            d = fuzzy.Single().Between(.2f, .5f);
-            d = fuzzy.Single().Not(.4f);
+        public void GetFuzzyValue() {
+            float value = fuzzy.Single();
         }
 
         [Fact]
-        public void GetArray() {
-            float[] d = fuzzy.Array(fuzzy.Single);
+        public void GetArrayOfFuzzyValues() {
+            float[] values = fuzzy.Array(fuzzy.Single);
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void ConstrainFuzzyValue() {
+            float value;
+            value = fuzzy.Single().GreaterThan(.2f);
+            value = fuzzy.Single().LessThan(.5f);
+            value = fuzzy.Single().Between(.2f, .5f);
+            value = fuzzy.Single().Not(.4f);
         }
     }
 }
