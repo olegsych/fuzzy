3ea4e7a7ed63dd3604ed2515d8beb5082bcbd2dc
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-12 14:30:41 -0700

    Define MVP DictionaryExample

---
+++ examples/DictionaryExample.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Fuzzy
+{
+    public class DictionaryExample
+    {
+        static readonly IFuzz fuzzy = new SequentialFuzz();
+
+        [Fact]
+        public void CreateDictionaryOfFuzzyElements() {
+            Dictionary<int, string> values = fuzzy.Dictionary(fuzzy.Int32, fuzzy.String);
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void CreateDictionaryOfFuzzyElementsWithCountSpecification() {
+            Dictionary<int, string> values = fuzzy.Dictionary(fuzzy.Int32, fuzzy.String, Count.Between(41, 43));
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void CreateDictionaryOfExistingElements() {
+            IEnumerable<KeyValuePair<int, string>> existing = new Dictionary<int, string> {
+                [41] = "foo", [42] = "bar", [43] = "baz"
+            };
+            Dictionary<int, string> selected = fuzzy.Dictionary(existing);
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void CreateDictionaryOfExistingElementsWithCountSpecification() {
+            IEnumerable<KeyValuePair<int, string>> existing = new Dictionary<int, string> {
+                [41] = "foo", [42] = "bar", [43] = "baz"
+            };
+            Dictionary<int, string> selected = fuzzy.Dictionary(existing, Count.Between(2, 3));
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void CreateDictionaryOfCustomElements() {
+            int key = Environment.TickCount;
+            Dictionary<int, string> values = fuzzy.Dictionary(() => key++, () => $"value{key}");
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void CreateArrayOfCustomElementsWithCountSpecification() {
+            int key = Environment.TickCount;
+            Dictionary<int, string> values = fuzzy.Dictionary(() => key++, () => $"value{key}", Count.Between(42, 43));
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void ControlDictionaryCount() {
+            DateTimeOffset[] dates;
+            dates = fuzzy.Array(fuzzy.DateTimeOffset, Length.Exactly(42));
+            dates = fuzzy.Array(fuzzy.DateTimeOffset, Length.Between(41, 43));
+            dates = fuzzy.Array(fuzzy.DateTimeOffset, Length.Max(43));
+            dates = fuzzy.Array(fuzzy.DateTimeOffset, Length.Min(5));
+        }
+    }
+}
---
+++ src/IFuzzDictionaryExtensions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Fuzzy
+{
+    public static class IFuzzDictionaryExtensions
+    {
+        public static Fuzzy<Dictionary<TKey, TValue>> Dictionary<TKey, TValue>(this IFuzz fuzzy, Func<TKey> createKey, Func<TValue> createValue, Count count = default) => throw new NotImplementedException();
+        public static Fuzzy<Dictionary<TKey, TValue>> Dictionary<TKey, TValue>(this IFuzz fuzzy, Func<Fuzzy<TKey>> createKey, Func<Fuzzy<TValue>> createValue, Count count = default) => throw new NotImplementedException();
+        public static Fuzzy<Dictionary<TKey, TValue>> Dictionary<TKey, TValue>(this IFuzz fuzzy, IEnumerable<KeyValuePair<TKey, TValue>> elements, Count count = default) => throw new NotImplementedException();
+    }
+}
