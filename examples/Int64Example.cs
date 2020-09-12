5520ca2e4438c27c12253fcd38afc6fa22e73dfd
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-12 11:43:21 -0700

    Define Int64Example MVP

--- examples/Int64Example.cs
+++ examples/Int64Example.cs
@@ -7,19 +7,23 @@ namespace Fuzzy
         static readonly IFuzz fuzzy = new SequentialFuzz();
 
         [Fact]
-        public void GetValue() {
-            long number;
-            number = fuzzy.Int64();
-            number = fuzzy.Int64().LessThan(42L);
-            number = fuzzy.Int64().GreaterThan(42L);
-            number = fuzzy.Int64().Between(41L, 43L);
-            number = fuzzy.Int64().GreaterThan(41L).LessThan(43L);
-            number = fuzzy.Int64().Not(42L);
+        public void GetFuzzyValue() {
+            long value = fuzzy.Int64();
         }
 
         [Fact]
-        public void GetArray() {
-            long[] numbers = fuzzy.Array(fuzzy.Int64);
+        public void GetArrayOfFuzzyValues() {
+            long[] values = fuzzy.Array(fuzzy.Int64);
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void ConstrainFuzzyValue() {
+            long value;
+            value = fuzzy.Int64().LessThan(42L);
+            value = fuzzy.Int64().GreaterThan(42L);
+            value = fuzzy.Int64().Between(41L, 43L);
+            value = fuzzy.Int64().GreaterThan(41L).LessThan(43L);
+            value = fuzzy.Int64().Not(42L);
         }
     }
 }
