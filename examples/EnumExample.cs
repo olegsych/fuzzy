10805456e69be19c053cfb07aaeb75d2e8c97973
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-12 11:27:21 -0700

    Define EnumExample MVP

--- examples/EnumExample.cs
+++ examples/EnumExample.cs
@@ -8,17 +8,22 @@ namespace Fuzzy
         static readonly IFuzz fuzzy = new SequentialFuzz();
 
         [Fact]
-        public void GetAnyEnum() {
-            TypeCode e = fuzzy.Enum<TypeCode>();
+        public void GetFuzzyValue() {
+            TypeCode value = fuzzy.Enum<TypeCode>();
         }
 
         [Fact]
-        public void ControlEnumValue() {
-            TypeCode e;
-            e = fuzzy.Enum<TypeCode>().Not(TypeCode.SByte);
-            e = fuzzy.Enum<TypeCode>().LessThan(TypeCode.UInt64).Not(TypeCode.SByte);
-            e = fuzzy.Enum<TypeCode>().GreaterThan(TypeCode.Boolean).Not(TypeCode.SByte);
-            e = fuzzy.Enum<TypeCode>().Between(TypeCode.Boolean, TypeCode.Int32).Not(TypeCode.SByte);
+        public void GetArrayOfFuzzyValues() {
+            TypeCode[] values = fuzzy.Array(fuzzy.Enum<TypeCode>);
+        }
+
+        [Fact(Skip = Reason.NotImplemented)]
+        public void ControlFuzzyValue() {
+            TypeCode value;
+            value = fuzzy.Enum<TypeCode>().Not(TypeCode.SByte);
+            value = fuzzy.Enum<TypeCode>().LessThan(TypeCode.UInt64).Not(TypeCode.SByte);
+            value = fuzzy.Enum<TypeCode>().GreaterThan(TypeCode.Boolean).Not(TypeCode.SByte);
+            value = fuzzy.Enum<TypeCode>().Between(TypeCode.Boolean, TypeCode.Int32).Not(TypeCode.SByte);
         }
     }
 }
