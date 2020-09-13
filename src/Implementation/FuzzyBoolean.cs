db40b513efdcf6356fdce919997a7bb28c008099
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-12 17:24:51 -0700

    Update FuzzyBoolean

--- src/Implementation/FuzzyBoolean.cs
+++ src/Implementation/FuzzyBoolean.cs
@@ -2,7 +2,7 @@ namespace Fuzzy.Implementation
 {
     sealed class FuzzyBoolean: Fuzzy<bool>
     {
-        public FuzzyBoolean(IFuzz fuzz) : base(fuzz) {}
+        public FuzzyBoolean(IFuzz fuzzy) : base(fuzzy) {}
         public override bool New() => fuzzy.Next() % 2 == 1;
     }
 }
