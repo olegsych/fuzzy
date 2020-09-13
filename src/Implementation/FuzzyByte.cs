02bf8fce9af6f0136f47e28257c965b927bdbb45
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-12 17:25:19 -0700

    Update FuzzyByte

--- src/Implementation/FuzzyByte.cs
+++ src/Implementation/FuzzyByte.cs
@@ -2,7 +2,7 @@ namespace Fuzzy.Implementation
 {
     sealed class FuzzyByte: Fuzzy<byte>
     {
-        public FuzzyByte(IFuzz fuzz) : base(fuzz) {}
+        public FuzzyByte(IFuzz fuzzy) : base(fuzzy) {}
         public override byte New() => (byte)(fuzzy.Next() % byte.MaxValue);
     }
 }
