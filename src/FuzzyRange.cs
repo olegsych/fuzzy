defafdf3ad9854baf26c1910f8104f89f9d6d7ef
Oleg Sych <olegsych@users.noreply.github.com>
2020-09-14 21:43:43 -0700

    Implement FuzzyRange

--- src/FuzzyRange.cs
+++ src/FuzzyRange.cs
@@ -1,16 +1,35 @@
 using System;
-using System.Collections.Generic;
 
 namespace Fuzzy
 {
-    public abstract class FuzzyRange<T>: Fuzzy<T> where T : struct, IComparable<T>, IEquatable<T>
+    public abstract class FuzzyRange<T>: Fuzzy<T> where T : struct, IComparable<T>
     {
-        public FuzzyRange(IFuzz fuzz) :
-            base(fuzz) =>
-            Included = new HashSet<T>();
+        T maximum;
+        T minimum;
 
-        public ISet<T> Included { get; }
-        public T? Maximum { get; set; }
-        public T? Minimum { get; set; }
+        public FuzzyRange(IFuzz fuzz, T minimum, T maximum) : base(fuzz) {
+            if(maximum.CompareTo(minimum) < 0)
+                throw new ArgumentOutOfRangeException(nameof(maximum), $"{maximum} is less than the {nameof(minimum)} value {minimum}");
+            this.maximum = maximum;
+            this.minimum = minimum;
+        }
+
+        public T Maximum {
+            get => maximum;
+            set {
+                if(value.CompareTo(minimum) < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"{value} is less than the {nameof(Minimum)} value {minimum}");
+                maximum = value;
+            }
+        }
+
+        public T Minimum {
+            get => minimum;
+            set {
+                if(value.CompareTo(maximum) > 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"{value} is greater than the {nameof(Maximum)} value {maximum}");
+                minimum = value;
+            }
+        }
     }
 }
--- test/FuzzyRangeTest.cs
+++ test/FuzzyRangeTest.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Reflection;
 using Inspector;
 using NSubstitute;
 using Xunit;
@@ -12,66 +12,95 @@ namespace Fuzzy
 
         // Constructor parameters
         readonly IFuzz fuzz = Substitute.For<IFuzz>();
+        readonly TestStruct minimum;
+        readonly TestStruct maximum;
 
-        public FuzzyRangeTest() =>
-            sut = Substitute.ForPartsOf<FuzzyRange<TestStruct>>(fuzz);
+        public FuzzyRangeTest() {
+            int minimumValue = random.Next();
+            minimum = new TestStruct(minimumValue);
+            maximum = new TestStruct(minimumValue + 1);
+            sut = Substitute.ForPartsOf<FuzzyRange<TestStruct>>(fuzz, minimum, maximum);
+        }
 
         public class Constructor: FuzzyRangeTest
         {
             [Fact]
             public void CallsBaseConstructor() =>
                 Assert.Same(fuzz, sut.DeclaredBy<Fuzzy<TestStruct>>().Field<IFuzz>().Value);
-        }
 
-        public class Included: FuzzyRangeTest
-        {
             [Fact]
-            public void IsEmptyByDefault() {
-                ISet<TestStruct> actual = sut.Included;
-                Assert.Equal(0, actual.Count);
+            public void ThrowsDescriptiveExceptionWhenMaximumIsLessThanMinimum() {
+                var invalid = new TestStruct(minimum.Value - 1);
+
+                var wrapped = Assert.Throws<TargetInvocationException>(() => Substitute.ForPartsOf<FuzzyRange<TestStruct>>(fuzz, minimum, invalid));
+
+                var thrown = Assert.IsType<ArgumentOutOfRangeException>(wrapped.InnerException);
+                Assert.Equal("maximum", thrown.ParamName);
+                Assert.Contains("minimum", thrown.Message);
+                Assert.Contains(invalid.ToString(), thrown.Message);
+                Assert.Contains(minimum.ToString(), thrown.Message);
             }
         }
 
         public class Maximum: FuzzyRangeTest
         {
             [Fact]
-            public void IsNullByDefault() {
-                TestStruct? actual = sut.Maximum;
-                Assert.False(actual.HasValue);
-            }
+            public void IsInitializedByConstructor() =>
+                Assert.Equal(maximum, sut.Maximum);
 
             [Fact]
             public void CanBeSet() {
-                var expected = new TestStruct(random.Next());
+                var expected = new TestStruct(minimum.Value + 1);
                 sut.Maximum = expected;
-                TestStruct? actual = sut.Maximum;
-                Assert.Equal(expected, actual);
+                Assert.Equal(expected, sut.Maximum);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenNewValueIsLessThanMinimum() {
+                int value = minimum.Value - 1;
+
+                var thrown = Assert.Throws<ArgumentOutOfRangeException>(() => sut.Maximum = new TestStruct(value));
+
+                Assert.Equal("value", thrown.ParamName);
+                Assert.Contains(nameof(sut.Minimum), thrown.Message);
+                Assert.Contains(value.ToString(), thrown.Message);
+                Assert.Contains(minimum.ToString(), thrown.Message);
             }
         }
 
         public class Minimum: FuzzyRangeTest
         {
             [Fact]
-            public void IsNullByDefault() {
-                TestStruct? actual = sut.Minimum;
-                Assert.False(actual.HasValue);
-            }
+            public void IsInitializedByConstructor() =>
+                Assert.Equal(minimum, sut.Minimum);
 
             [Fact]
             public void CanBeSet() {
-                var expected = new TestStruct(random.Next());
+                var expected = new TestStruct(maximum.Value - 1);
                 sut.Minimum = expected;
-                TestStruct? actual = sut.Minimum;
-                Assert.Equal(expected, actual);
+                Assert.Equal(expected, sut.Minimum);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenNewValueIsGreaterThanMaximum() {
+                int value = maximum.Value + 1;
+
+                var thrown = Assert.Throws<ArgumentOutOfRangeException>(() => sut.Minimum = new TestStruct(value));
+
+                Assert.Equal("value", thrown.ParamName);
+                Assert.Contains(nameof(sut.Maximum), thrown.Message);
+                Assert.Contains(value.ToString(), thrown.Message);
+                Assert.Contains(maximum.ToString(), thrown.Message);
             }
         }
 
-        public struct TestStruct: IComparable<TestStruct>, IEquatable<TestStruct>
+        public struct TestStruct: IComparable<TestStruct>
         {
-            readonly int value;
-            public TestStruct(int value) => this.value = value;
-            public int CompareTo(TestStruct other) => value.CompareTo(other.value);
-            public bool Equals(TestStruct other) => value.Equals(other.value);
+            public readonly int Value;
+            public TestStruct(int value) => Value = value;
+            public int CompareTo(TestStruct other) => Value.CompareTo(other.Value);
+            public bool Equals(TestStruct other) => Value.Equals(other.Value);
+            public override string ToString() => Value.ToString();
         }
 
         static readonly Random random = new Random();
