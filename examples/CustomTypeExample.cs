540b1e035da13376b537947c0995d1ceff92b07a
Oleg Sych <olegsych@users.noreply.github.com>
2020-10-18 12:31:39 -0700

    Create CustomTypeExample

---
+++ examples/CustomTypeExample.cs
@@ -0,0 +1,29 @@
+using Xunit;
+
+namespace Fuzzy
+{
+    class CustomType
+    {
+        public int Foo;
+        public string Bar;
+    }
+
+    static class IFuzzExtensions
+    {
+        public static CustomType CustomType(this IFuzz fuzzy) =>
+            new CustomType {
+                Foo = fuzzy.Int32(),
+                Bar = fuzzy.String(),
+            };
+    }
+
+    public class CustomTypeExample
+    {
+        static readonly IFuzz fuzzy = new RandomFuzz();
+
+        [Fact]
+        public void GetFuzzyValue() {
+            CustomType value = fuzzy.CustomType();
+        }
+    }
+}
