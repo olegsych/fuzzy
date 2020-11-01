8e3406af85c407dc75ad2ef13df12d65a3a27efb
Oleg Sych <olegsych@users.noreply.github.com>
2020-10-31 17:50:56 -0700

    Remove min/max from SingleExample

--- examples/SingleExample.cs
+++ examples/SingleExample.cs
@@ -10,14 +10,5 @@ namespace Fuzzy
         public void GetFuzzyValue() {
             float value = fuzzy.Single();
         }
-
-        [Fact(Skip = Reason.NotImplemented)]
-        public void ConstrainFuzzyValue() {
-            float value;
-            value = fuzzy.Single().GreaterThan(.2f);
-            value = fuzzy.Single().LessThan(.5f);
-            value = fuzzy.Single().Between(.2f, .5f);
-            value = fuzzy.Single().Not(.4f);
-        }
     }
 }
