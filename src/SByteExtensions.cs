bba37e6080627a8a0f7c5d7ed97c8332560a13d9
Oleg Sych <olegsych@users.noreply.github.com>
2020-10-31 21:15:11 -0700

    Make SByteExtensions use sbyte instead of FuzzyRange<sbyte>

--- src/SByteExtensions.cs
+++ src/SByteExtensions.cs
@@ -1,12 +1,12 @@
 namespace Fuzzy
 {
     /// <remarks>
-    /// Needed because compiler cannot resolve <see cref="FuzzyRangeExtensions"/> calls made with literal numbers.
+    /// Needed because compiler cannot resolve <see cref="IComparableExtensions"/> calls made with literal numbers.
     /// </remarks>
     public static class SByteExtensions
     {
-        public static FuzzyRange<sbyte> Between(this FuzzyRange<sbyte> value, sbyte minimum, sbyte maximum) => FuzzyRangeExtensions.Between(value, minimum, maximum);
-        public static FuzzyRange<sbyte> Maximum(this FuzzyRange<sbyte> value, sbyte maximum) => FuzzyRangeExtensions.Maximum(value, maximum);
-        public static FuzzyRange<sbyte> Minimum(this FuzzyRange<sbyte> value, sbyte minimum) => FuzzyRangeExtensions.Minimum(value, minimum);
+        public static sbyte Between(this sbyte value, sbyte minimum, sbyte maximum) => IComparableExtensions.Between(value, minimum, maximum);
+        public static sbyte Maximum(this sbyte value, sbyte maximum) => IComparableExtensions.Maximum(value, maximum);
+        public static sbyte Minimum(this sbyte value, sbyte minimum) => IComparableExtensions.Minimum(value, minimum);
     }
 }
--- test/SByteExtensionsTest.cs
+++ test/SByteExtensionsTest.cs
@@ -1,70 +1,59 @@
-using System;
+using Fuzzy.Implementation;
 using NSubstitute;
+using NSubstitute.Core;
 using Xunit;
 
 namespace Fuzzy
 {
-    public class SByteExtensionsTest
+    public class SByteExtensionsTest: TestFixture
     {
         // Method parameters
-        readonly FuzzyRange<sbyte> value;
+        readonly sbyte value = (sbyte)(random.Next() % 64);
         readonly sbyte minimum = (sbyte)(sbyte.MinValue + random.Next() % 64);
         readonly sbyte maximum = (sbyte)(sbyte.MaxValue - random.Next() % 64);
 
-        // Shared test fixture
-        static readonly Random random = new Random();
-        readonly FuzzyRange<sbyte> @null = null;
-        readonly IFuzz fuzzy = Substitute.For<IFuzz>();
+        // Test fixture
+        readonly FuzzyRange<sbyte> spec;
+        readonly sbyte newValue = (sbyte)(random.Next() % sbyte.MaxValue);
 
-        public SByteExtensionsTest() =>
-            value = Substitute.ForPartsOf<FuzzyRange<sbyte>>(fuzzy, sbyte.MinValue, sbyte.MaxValue);
+        public SByteExtensionsTest() {
+            spec = Substitute.ForPartsOf<FuzzyRange<sbyte>>(fuzzy, sbyte.MinValue, sbyte.MaxValue);
+
+            FuzzyContext.Set(value, spec);
+            ConfiguredCall arrange = fuzzy.Build(spec).Returns(newValue);
+        }
 
         public class Between: SByteExtensionsTest
         {
             [Fact]
-            public void ThrowsDescriptiveExceptionWhenValueIsNull() {
-                var thrown = Assert.Throws<ArgumentNullException>(() => @null.Between(minimum, maximum));
-                Assert.Equal("value", thrown.ParamName);
-            }
-
-            [Fact]
             public void ReturnsValueWithMinimumAndMaximumPropertiesSet() {
-                FuzzyRange<sbyte> returned = value.Between(minimum, maximum);
-                Assert.Same(value, returned);
-                Assert.Equal(minimum, returned.Minimum);
-                Assert.Equal(maximum, returned.Maximum);
+                sbyte returned = value.Between(minimum, maximum);
+
+                Assert.Equal(newValue, returned);
+                Assert.Equal(minimum, spec.Minimum);
+                Assert.Equal(maximum, spec.Maximum);
             }
         }
 
         public class Maximum: SByteExtensionsTest
         {
             [Fact]
-            public void ThrowsDescriptiveExceptionWhenValueIsNull() {
-                var thrown = Assert.Throws<ArgumentNullException>(() => @null.Maximum(maximum));
-                Assert.Equal("value", thrown.ParamName);
-            }
-
-            [Fact]
             public void ReturnsValueWithMaximumPropertySet() {
-                FuzzyRange<sbyte> returned = value.Maximum(maximum);
-                Assert.Same(value, returned);
-                Assert.Equal(maximum, returned.Maximum);
+                sbyte returned = value.Maximum(maximum);
+
+                Assert.Equal(newValue, returned);
+                Assert.Equal(maximum, spec.Maximum);
             }
         }
 
         public class Minimum: SByteExtensionsTest
         {
             [Fact]
-            public void ThrowsDescriptiveExceptionWhenValueIsNull() {
-                var thrown = Assert.Throws<ArgumentNullException>(() => @null.Minimum(maximum));
-                Assert.Equal("value", thrown.ParamName);
-            }
-
-            [Fact]
             public void ReturnsValueWithMinimumPropertySet() {
-                FuzzyRange<sbyte> returned = value.Minimum(minimum);
-                Assert.Same(value, returned);
-                Assert.Equal(minimum, returned.Minimum);
+                sbyte returned = value.Minimum(minimum);
+
+                Assert.Equal(newValue, returned);
+                Assert.Equal(minimum, spec.Minimum);
             }
         }
     }
