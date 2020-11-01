4f74c0b89ff2a37deea54d8650c42e0994150481
Oleg Sych <olegsych@users.noreply.github.com>
2020-10-31 19:42:08 -0700

    Remove constraints from EnumExample

--- examples/EnumExample.cs
+++ examples/EnumExample.cs
@@ -11,14 +11,5 @@ namespace Fuzzy
         public void GetFuzzyValue() {
             TypeCode value = fuzzy.Enum<TypeCode>();
         }
-
-        [Fact(Skip = Reason.NotImplemented)]
-        public void ControlFuzzyValue() {
-            TypeCode value;
-            value = fuzzy.Enum<TypeCode>().Not(TypeCode.SByte);
-            value = fuzzy.Enum<TypeCode>().LessThan(TypeCode.UInt64).Not(TypeCode.SByte);
-            value = fuzzy.Enum<TypeCode>().GreaterThan(TypeCode.Boolean).Not(TypeCode.SByte);
-            value = fuzzy.Enum<TypeCode>().Between(TypeCode.Boolean, TypeCode.Int32).Not(TypeCode.SByte);
-        }
     }
 }
