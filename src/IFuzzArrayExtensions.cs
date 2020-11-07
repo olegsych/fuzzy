b85b15c9e11126793c26472a649f44c64e6d6757
Oleg Sych <olegsych@users.noreply.github.com>
2020-11-07 12:06:30 -0800

    Remove IFuzzArrayExtensions taking Fuzzy<> and FuzzyRange<>

--- src/IFuzzArrayExtensions.cs
+++ src/IFuzzArrayExtensions.cs
@@ -9,12 +9,6 @@ namespace Fuzzy
         public static FuzzyArray<T> Array<T>(this IFuzz fuzzy, Func<T> createElement, Length length = default) =>
             new FuzzyArray<T>(fuzzy, createElement, length ?? new Length());
 
-        public static FuzzyArray<T> Array<T>(this IFuzz fuzzy, Func<Fuzzy<T>> createElement, Length length = default) =>
-            fuzzy.Array(() => (T)createElement(), length);
-
-        public static FuzzyArray<T> Array<T>(this IFuzz fuzzy, Func<FuzzyRange<T>> createElement, Length length = default) where T : struct, IComparable<T> =>
-            fuzzy.Array((Func<Fuzzy<T>>)createElement, length);
-
         public static FuzzyArray<T> Array<T>(this IFuzz fuzzy, IEnumerable<T> elements, Length length = default) =>
             fuzzy.Array(() => fuzzy.Element(elements), length);
     }
