407d9f2072fce87d33dc5f53ae5fefe2da13a12d
Oleg Sych <olegsych@users.noreply.github.com>
2021-02-14 15:38:43 -0800

    Make Fuzzy types declare parameters accepting null values as nullable

--- src/IFuzzDictionaryExtensions.cs
+++ src/IFuzzDictionaryExtensions.cs
@@ -7,13 +7,13 @@ namespace Fuzzy
 {
     public static class IFuzzDictionaryExtensions
     {
-        public static Dictionary<TKey, TValue> Dictionary<TKey, TValue>(this IFuzz fuzzy, Func<TKey> createKey, Func<TValue> createValue, Count count = default)
+        public static Dictionary<TKey, TValue> Dictionary<TKey, TValue>(this IFuzz fuzzy, Func<TKey> createKey, Func<TValue> createValue, Count? count = default)
             => fuzzy.Dictionary(createKey, k => createValue(), count);
 
-        public static Dictionary<TKey, TValue> Dictionary<TKey, TValue>(this IFuzz fuzzy, Func<TKey> createKey, Func<TKey, TValue> createValue, Count count = default)
+        public static Dictionary<TKey, TValue> Dictionary<TKey, TValue>(this IFuzz fuzzy, Func<TKey> createKey, Func<TKey, TValue> createValue, Count? count = default)
             => new FuzzyDictionary<TKey, TValue>(fuzzy, createKey, createValue, count ?? new Count());
 
-        public static Dictionary<TKey, TValue> Dictionary<TKey, TValue>(this IFuzz fuzzy, IReadOnlyDictionary<TKey, TValue> elements, Count count = default)
+        public static Dictionary<TKey, TValue> Dictionary<TKey, TValue>(this IFuzz fuzzy, IReadOnlyDictionary<TKey, TValue> elements, Count? count = default)
             => fuzzy.Dictionary(() => fuzzy.Element(elements).Key, k => elements[k], count);
     }
 }
--- src/IFuzzListExtensions.cs
+++ src/IFuzzListExtensions.cs
@@ -6,10 +6,10 @@ namespace Fuzzy
 {
     public static class IFuzzListExtensions
     {
-        public static List<T> List<T>(this IFuzz fuzzy, Func<T> createElement, Count count = default) =>
+        public static List<T> List<T>(this IFuzz fuzzy, Func<T> createElement, Count? count = default) =>
             new FuzzyList<T>(fuzzy, createElement, count ?? new Count());
 
-        public static List<T> List<T>(this IFuzz fuzzy, IEnumerable<T> elements, Count count = default) =>
+        public static List<T> List<T>(this IFuzz fuzzy, IEnumerable<T> elements, Count? count = default) =>
             fuzzy.List(() => fuzzy.Element(elements), count);
     }
 }
