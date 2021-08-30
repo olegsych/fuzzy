4fd11dd2e1ca6a2b65d16c37ce80bf20b6fd6802
Oleg Sych <olegsych@users.noreply.github.com>
2021-08-29 18:35:26 -0700

    Fix CA1725 in Size.Equals()

--- src/Size.cs
+++ src/Size.cs
@@ -11,8 +11,8 @@ namespace Fuzzy
         int? maximum;
         int? minimum;
 
-        public override bool Equals(object other) =>
-            other is Size<TSize> otherSize &&
+        public override bool Equals(object obj) =>
+            obj is Size<TSize> otherSize &&
             otherSize.minimum.Equals(minimum) &&
             otherSize.maximum.Equals(maximum);
 
