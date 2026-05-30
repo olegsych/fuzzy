0b7337ee496b5a1c52fe27aa45f0eb9437d0b93c
Oleg Sych <olegsych@users.noreply.github.com>
2026-05-30 12:13:40 -0700

    src/Implementation/FuzzyRange.cs
    Ensure `Fuzzy.Implementation.FuzzyRange<T> in src/Implementation/FuzzyRange.cs` has complete and accurate XML documentation comments for all public members.

--- src/Implementation/FuzzyRange.cs
+++ src/Implementation/FuzzyRange.cs
@@ -2,11 +2,19 @@ using System;
 
 namespace Fuzzy.Implementation
 {
+    /// <summary>
+    /// Provides a base class for <see cref="Fuzzy{T}"/> specifications that produce values within an inclusive range.
+    /// </summary>
     public abstract class FuzzyRange<T>: Fuzzy<T> where T : struct, IComparable<T>
     {
         T maximum;
         T minimum;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FuzzyRange{T}"/> class.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"><paramref name="fuzz"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="maximum"/> is less than <paramref name="minimum"/>.</exception>
         public FuzzyRange(IFuzz fuzz, T minimum, T maximum) : base(fuzz) {
             if(maximum.CompareTo(minimum) < 0)
                 throw new ArgumentOutOfRangeException(nameof(maximum), $"{maximum} is less than the {nameof(minimum)} value {minimum}");
@@ -14,6 +22,10 @@ namespace Fuzzy.Implementation
             this.minimum = minimum;
         }
 
+        /// <summary>
+        /// Gets or sets the inclusive upper bound of the range.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">The assigned value is less than <see cref="Minimum"/>.</exception>
         public T Maximum {
             get => maximum;
             set {
@@ -23,6 +35,10 @@ namespace Fuzzy.Implementation
             }
         }
 
+        /// <summary>
+        /// Gets or sets the inclusive lower bound of the range.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">The assigned value is greater than <see cref="Maximum"/>.</exception>
         public T Minimum {
             get => minimum;
             set {
