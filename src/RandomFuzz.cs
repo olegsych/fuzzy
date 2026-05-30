6173caa6a8e14625f176634e47556cd8bbb335a9
Oleg Sych <olegsych@users.noreply.github.com>
2026-05-30 11:16:27 -0700

    src/RandomFuzz.cs
    Implement complete and accurate XML documentation comments for all public members of `Fuzzy.RandomFuzz` in `src/RandomFuzz.cs`.
    Note that I'm working on the following request.
    ```
    Ensure `Fuzzy.RandomFuzz in src/RandomFuzz.cs` has complete and accurate XML documentation comments for all public members.
    API, functional, style changes are out of scope.
    ```

--- src/RandomFuzz.cs
+++ src/RandomFuzz.cs
@@ -2,12 +2,20 @@ using System;
 
 namespace Fuzzy
 {
+    /// <summary>
+    /// Produces fuzzy <see langword="int"/> values from a pseudo-random sequence.
+    /// </summary>
     public class RandomFuzz : Implementation.Fuzz
     {
         readonly Random random;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomFuzz"/> class.
+        /// </summary>
+        /// <param name="seed">A seed for the underlying <see cref="Random"/>, enabling reproducible sequences.</param>
         public RandomFuzz(int seed = 0) => random = new Random(seed);
 
+        /// <inheritdoc/>
         public override int Next() => random.Next();
     }
 }
