bee08dfefc03c55c2158c79aa65cb700c495438b
Oleg Sych <olegsych@users.noreply.github.com>
2026-05-30 15:50:10 -0700

    src/UInt16Extensions.cs

--- src/UInt16Extensions.cs
+++ src/UInt16Extensions.cs
@@ -1,12 +1,16 @@
 namespace Fuzzy
 {
-    /// <remarks>
-    /// Needed because compiler cannot resolve <see cref="IComparableExtensions"/> calls made with literal numbers.
-    /// </remarks>
+    /// <summary>Provides range constraints for fuzzy <see langword="ushort"/> values.</summary>
+    // Needed because compiler cannot resolve IComparableExtensions calls made with literal numbers.
     public static class UInt16Extensions
     {
+        /// <inheritdoc cref="IComparableExtensions.Between{T}(T, T, T)"/>
         public static ushort Between(this ushort value, ushort minimum, ushort maximum) => IComparableExtensions.Between(value, minimum, maximum);
+
+        /// <inheritdoc cref="IComparableExtensions.Maximum{T}(T, T)"/>
         public static ushort Maximum(this ushort value, ushort maximum) => IComparableExtensions.Maximum(value, maximum);
+
+        /// <inheritdoc cref="IComparableExtensions.Minimum{T}(T, T)"/>
         public static ushort Minimum(this ushort value, ushort minimum) => IComparableExtensions.Minimum(value, minimum);
     }
 }
